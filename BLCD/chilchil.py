188d77772e59ffeb496e6e865b4d6e847fdf8f92
wuw2135 <80589831+wuw2135@users.noreply.github.com>
2021-04-19 21:40:17 +0900

    Update chilchil.py

--- BLCD/chilchil.py
+++ BLCD/chilchil.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
-import requests
-from fake_useragent import UserAgent
 import time
 import datetime
 import json
