bd48886261d2fb8ef94459e2f029ab6f85bb18f9
wuw2135 <80589831+wuw2135@users.noreply.github.com>
2021-04-21 00:08:45 +0900

    Update chilchil.py

--- BLCD/chilchil.py
+++ BLCD/chilchil.py
@@ -9,7 +9,7 @@ import linecache
 
 options = webdriver.ChromeOptions()
 options.add_argument('user-agent="Mozilla/5.0 (iPod; U; CPU iPhone OS 2_1 like Mac OS X; ja-jp) AppleWebKit/525.18.1 (KHTML, like Gecko) Version/3.1.1 Mobile/5F137 Safari/525.20"')
-driver = webdriver.Chrome(chrome_options=options,executable_path="D:/notifiction_bot/SEIYUU/chromedriver")
+driver = webdriver.Chrome(chrome_options=options)
 
 #ACCOUNT SETTING
 def login():
